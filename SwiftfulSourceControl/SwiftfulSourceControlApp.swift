8e49f447d0d4a50ec77fef1a6a0fed615e9a927c
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-03-26 22:01:34 -0400

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Alvaro Ordonez on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
