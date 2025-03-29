8b9587e3adc1c6e61a864ae2a0ee30e2ab24c015
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-03-29 13:33:56 -0400

    [Comments] Added comments on merging

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Alvaro Ordonez on 3/26/25.
 //
 
+//NOTE! If I want to merge mysecondbranch into main, from the SourceControl tab on the navigation pane(on the left), check out main branch(right click, Switch to main), right click on mysecondbranch, click merge mysecondbranch into main.
+
 import SwiftUI
 
 struct ContentView: View {
