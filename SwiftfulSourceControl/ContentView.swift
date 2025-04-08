30c337598b7004c6dd8fc050bc5755edb7463639
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-04-08 15:13:26 -0400

    [Feature] Added SettingsView

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -7,6 +7,8 @@
 
 //NOTE! If I want to merge mysecondbranch into main, from the SourceControl tab on the navigation pane(on the left), check out main branch(right click, Switch to main), right click on mysecondbranch, click merge mysecondbranch into main. Then you need to Cmd-Option-C, write comments, Stage All and commit locally. Then need to push the changes on to the remote repo.
 
+//Pull Request(PR) Command to merge a branch
+
 import SwiftUI
 
 struct ContentView: View {
---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Alvaro Ordonez on 4/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
