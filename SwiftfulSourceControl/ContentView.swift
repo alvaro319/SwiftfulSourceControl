d416d612b32e64d15c5f6309a25c96a72bf3c9e9
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-04-10 12:53:55 -0400

    [Clean] Increment build version to 0.0.1

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -9,6 +9,58 @@
 
 //Pull Request(PR) Command to merge a branch
 
+/*
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") changes on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+ Merge = Joining two different branch
+ Rebase = Moving one branch on top of another branch
+ Cherry Picking = Duplicating (copying) one commit from one branch to another
+ Pull Request (PR) = Request to merge branch
+ PR Merge = Merge all commits
+ PR Squash and Merge = Squash all commits into ONE and then merge ONE commit
+ Protecting Branches
+ CODEOWNERS
+ GitIgnore
+ ReadMe
+ Releases, Tags, Versioning
+ 
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ RELEASE:
+ [Release] Description of release
+
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ Versioning strategy:
+ 
+ 0.0.1
+ 
+ First 0 - Major (For big updates)
+ Second 0 - Minor (Minor/regular updates)
+ Third 1 - Patch (Bug Fixes)
+ 
+ Build Strategy:
+ 
+ With every change in either minor, or patch, the build number increases. If Major increases, the Build number resets back to 1.
+ 
+    Or the build number goes up with any change in the version number(even of major number changes)
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
