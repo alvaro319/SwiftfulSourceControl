997f229fe1064a6f7d1a885c30dce2abbfe15f28
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-04-11 23:23:49 -0400

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Alvaro Ordonez on 4/11/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Alvaro!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
